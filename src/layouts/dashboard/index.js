764590aa5302c3b67b57ec5c5396ebbc2b2bd694
sadikhossain69 <sadikhossain433@gmail.com>
2022-11-13 23:11:57 +0600

    dynamically rendering icon

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,22 +1,39 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, IconButton, Stack, useTheme } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Logo from '../../assets/Images/logo.ico';
+import { Nav_Buttons } from "../../data";
 
 const DashboardLayout = () => {
 
   const theme = useTheme()
 
-  console.log(theme)
-
   return (
     <>
-      <Box sx={{
+      <Box p={2} sx={{
         backgroundColor: theme.palette.background.paper,
         boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
         height: '100vh',
         width: 100
       }}>
-
+        <Stack direction={'column'} alignItems={"center"} sx={{ width: "100%" }}>
+          <Box sx={{
+            backgroundColor: theme.palette.primary.main,
+            height: 64,
+            width: 64,
+            borderRadius: 1.5,
+          }}>
+            <img src={Logo} alt="Let's Chat App Logo" />
+          </Box>
+          {
+            Nav_Buttons.map((el) =>
+              <IconButton
+                key={el.index}
+              >
+                {el.icon}
+              </IconButton>)
+          }
+        </Stack>
       </Box>
       <Outlet />
     </>
